750f52f7d64724bc3b4c9d4a3a99a43c1d39176f
XMaster96 <XMaster96@gmx.de>
2021-02-15 19:27:18 +0100

    Update run_experiments.py

--- run_experiments.py
+++ run_experiments.py
@@ -1,9 +1,10 @@
+import subprocess
 import argparse
 import hashlib
 import json
-import os
-import subprocess
+import math
 import time
+import os
 
 
 def str2bool(v: str) -> bool:
@@ -36,6 +37,7 @@ if __name__ == '__main__':
     parser.add_argument('--use_preemptible', type=str, default='true')
     parser.add_argument('--tpu_type', type=str, default='v3-8')
     parser.add_argument('--start_up_sleep', type=int, default=0)
+    parser.add_argument('--project', type=str, default='mlops-engine')
 
     args = parser.parse_args()
 
@@ -75,6 +77,15 @@ if __name__ == '__main__':
         for repetition_idx in range(args.repetition_start_idx, args.number_of_repetitions):
             tpu_name = f"tpu-{tpu_type}-euw4a-{tpu_id}" + args.tpu_name_subfix
 
+            cors = str(tpu_type).split('-')[-1]
+            if cors == 8:
+                tpu_range = f"10.48.{tpu_id}.0/29"
+            else:
+                cidr = int(32 + 2 - math.log2(cors))
+                _tpu_id = tpu_id + 2
+
+                tpu_range = f"10.{_tpu_id}.0.0/{cidr}"
+
             run_name = f"-run={repetition_idx}"
             run_name = "-".join([f"{key}={copy_base_config[key]}" for key in run_config_key]) + run_name
             run_name = run_name.replace(' ', '_').replace("'", '').replace(":", '=').replace(",", '-')
@@ -87,8 +98,8 @@ if __name__ == '__main__':
             experiment_command = f"python3 main.py --model buffer_configs/{tpu_id}.json --tpu {tpu_name}"
             delete_command = f"pu delete {tpu_name} --yes"
             tpu_creat_command = f"gcloud compute tpus create {tpu_name} --zone europe-west4-a " \
-                                f"--range 10.48.{tpu_id}.0/29 --network tpu-euw4a --version 1.15.4 " \
-                                f"--accelerator-type {tpu_type_str}"
+                                f"--range {range} --network tpu-euw4a --version 1.15.5 " \
+                                f"--accelerator-type {tpu_type_str} --project {args.project}"
 
             if str2bool(args.use_preemptible):
                 tpu_creat_command = tpu_creat_command + " --preemptible"
