24b9a7c2e4575f64399c1c4ec159e8853815573b
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-03-02 18:07:42 +0100

    fix(dataclass): add grad-accum param

--- src/dataclass.py
+++ src/dataclass.py
@@ -73,6 +73,7 @@ class ModelParameter(typing.Dict[str, typing.Any]):
                                                  }
         self.language_token_per_frame = 0
         self.weight_decay = 0.1
+        self.grad_accumulation = 1
         self.train_steps = 150_000
         self.warmup_steps = 3000
         self.learning_rate_decay_multi = 1
