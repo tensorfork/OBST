fcd146671ec6d8662a39ef6463bf20672c0c1d3a
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-03-12 16:10:36 +0100

    perf(train): add more optimization options

--- src/inputs.py
+++ src/inputs.py
@@ -5,14 +5,12 @@ import logging
 import random
 import re
 from itertools import cycle
-from functools import partial
 
 import tensorflow.compat.v1 as tf
-from tensorflow import contrib as tf_contrib
 from tensorflow.data import Dataset
 
 from .dataclass import ModelParameter, align_tensor_op
-
+import tensorflow as tf2
 
 def split_files(path, slice_index, slice_count, seed):
     filenames = tf.io.gfile.glob(path)
@@ -78,11 +76,6 @@ def get_video_decoder(language_token_num_per_frame=0, frame_height=None, frame_w
     return tf.function(frame_decoder, experimental_compile=False)
 
 
-def _read_records(decoder, filenames: tf.Tensor):
-    buffer_size = 8 * 1024 * 1024  # 8 MiB per file
-    return tf.data.TFRecordDataset(filenames=filenames, buffer_size=buffer_size)
-
-
 def _text_decoder(decoder, data: tf.Tensor, ctx: int, patch_size: int, chunk_size: int):
     """
     Read a given tfrecord and windowed text dataset out of it.
@@ -101,7 +94,6 @@ def _text_decoder(decoder, data: tf.Tensor, ctx: int, patch_size: int, chunk_siz
         return data
 
     return tf.data.TFRecordDataset(filenames=data).interleave(chunk)
-
 
 
 @tf.function
@@ -400,7 +392,7 @@ def _get_skip_index(all_files, n_batches):
     return skip_idx, remainder
 
 
-def gpt_neo_input_old(params, sub_batch_size, slice_index, slice_count):
+def gpt_neo_input(params, sub_batch_size, slice_index, slice_count):
     """
     Input fn that reads tfrecords encoded with a fixed chunk size (== n_ctx + 1), and that either:
 
@@ -446,7 +438,9 @@ def gpt_neo_input_old(params, sub_batch_size, slice_index, slice_count):
         return {'token_x': vals1, 'token_y': vals2}
 
     decoder = decode_intstring if 'int64' in filenames[0] else decode_bytestring
-    dset = dset.interleave(lambda x: _text_decoder(decoder, x, params.n_ctx, params.token_patch_size, -1))
+    dset = dset.interleave(lambda x: _text_decoder(decoder, x, params.n_ctx, params.token_patch_size, -1),
+                           cycle_length=params.interleaved_datasets,
+                           num_parallel_calls=tf.data.experimental.AUTOTUNE)
 
     dset = dset.shuffle(params.shuffle_buffer, seed=params.data_seed)
     dset = dset.batch(sub_batch_size)
@@ -454,341 +448,24 @@ def gpt_neo_input_old(params, sub_batch_size, slice_index, slice_count):
     dset = dset.map(_memory_func)
     dset = dset.map(align_tensor_op)
 
-    return dset
-
-
-
-def _text_decoder_new(decoder, data: tf.Tensor, ctx: int, patch_size: int, chunk_size: int):
-    """
-    Read a given tfrecord and windowed text dataset out of it.
-    :param data: protobuf object to decode
-    :param ctx: context size of generated dataset
-    :param chunk_size: batch size directly after creating the dataset
-    :return: tensorflow dataset of token
-    """
-
-    def chunk(tfrecorddataset):
-        data = decoder(tfrecorddataset)
-        if chunk_size > 0:
-            data = data.batch(chunk_size)
-        data = data.window(size=ctx + patch_size, shift=ctx, stride=1, drop_remainder=True)
-        data = data.interleave(lambda x: x.batch(ctx + patch_size, drop_remainder=True))
-        return data
-
-    return data.interleave(chunk)
-
-@tf.function
-def decode_bytestring_static(proto):
-    text_slice = tf.parse_single_example(proto, {'text': tf.FixedLenFeature([], tf.string)})['text']
-    return tf.strings.unicode_decode(text_slice, 'UTF-8')
-
-
-@tf.function
-def decode_intstring_static(proto):
-    x = tf.parse_single_example(proto, {'text': tf.VarLenFeature(tf.int64)})
-    x = x['text']
-    x = tf.sparse.to_dense(x)
-    x = tf.cast(x, tf.int32)
-    return x
-
-def ds_set_shape(ds, shape):
-  def inner(x):
-    x.set_shape(shape)
-    return x
-  return ds.map(inner)
-
-
-def ds_chunkify(ds, chunk_size):
-  ds = ds.unbatch()
-  ds = ds.batch(chunk_size)
-  ds = ds_set_shape(ds, [chunk_size])
-  return ds
-
-
-# def randi(maxval, shape=[], dtype=tf.dtypes.int32):
-#   return tf.random.uniform(shape, maxval=maxval, dtype=dtype)
-
-# def verify(op_name, *args, **kws):
-#   fn = getattr(tf.debugging, 'assert_' + op_name)
-#   op = fn(*args, **kws)
-#   return tf.control_dependencies([op])
-
-# verify_less_eq = partial(verify, "less_equal")
-
-# def randrange(haystack_size, needle_size, batch_size=None, dtype=tf.dtypes.int32):
-#   with verify_less_eq(needle_size, haystack_size):
-#     if batch_size is not None:
-#       shape = [batch_size]
-#     else:
-#       shape = []
-#     return tf.range(randi(haystack_size - needle_size, shape=shape, dtype=dtype))
-
-
-
-def verify(op_name, *args, **kws):
-  fn = getattr(tf.debugging, 'assert_' + op_name)
-  op = fn(*args, **kws)
-  return tf.control_dependencies([op])
-
-verify_less_eq = partial(verify, "less_equal")
-
-
-def shapeof(shape=None):
-  if hasattr(shape, 'shape'):
-    shape = shape.shape
-  if isinstance(shape, tf.TensorShape):
-    shape = shape.as_list()
-  if shape is None:
-    shape = [1]
-  if isinstance(shape, int):
-    shape = [shape]
-  assert isinstance(shape, (list, tuple))
-  return shape
-
-# given a bin that holds `total` elements, return a random
-# position such that you can take the next `subset` elements
-# without going out of bounds. E.g. randpos(1,10) will return
-# [0..9], randpos(2,10) will return [0..8], etc.
-def randpos(subset, total, shape=[1], *, dtype=tf.int64):
-  shape = shapeof(shape)
-  #assert subset <= total
-  with verify_less_eq(subset, total):
-    return tf.random.uniform(shape=shape, maxval=(total - subset)+1, dtype=dtype)
-
-def randrange(subset, total, shape=[1], *, dtype=tf.int64):
-  subset = tf.saturate_cast(subset, dtype)
-  total = tf.saturate_cast(total, dtype)
-  shape = shapeof(shape)
-  pos = randpos(subset, total, shape=shape, dtype=dtype)
-  part = tf.tile(tf.expand_dims(tf.range(subset, dtype=dtype), axis=0), shape + [1])
-  indices = part + tf.expand_dims(pos, axis=1)
-  return indices
-
-def sample_from(x, amount, shape=[1]):
-  x = tf.convert_to_tensor(x)
-  if x.dtype == tf.string:
-    x = tf.strings.unicode_decode(x, 'utf-8')
-    x = tf.cast(x, tf.int32)
-  shape = shapeof(shape)
-  n = tf.size(x)
-  indices = randrange(amount, n, shape=shape)
-  return tf.gather(x, indices)
-  
-
-import os
-homepath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-def random_text():
-  with open(os.path.join(homepath, 'LICENSE')) as f:
-    return f.read()
-
-def sample_random_text(n_ctx, batch_size, text=None):
-  if text is None:
-    text = random_text()
-  assert len(text) >= n_ctx
-  chars = tf.strings.unicode_decode(text, 'utf-8')
-  tokens = tf.saturate_cast(chars, tf.int32)
-  return sample_from(tokens, n_ctx, batch_size)
-
-
-# Sample 1024(+1) tokens from the stitched together text
-def sample_text(x, amount, maxlen=None):
-    if maxlen is None:
-      maxlen = tf.size(x)
-    with verify_less_eq(amount, maxlen):
-      r = randi(maxlen-amount)
-    r = tf.range(r, r+amount)
-    r = tf.reshape(r, [amount])
-    return tf.gather(x, r)
-
-from functools import partial
-
-def gpt_random_input(params, sub_batch_size, slice_index, slice_count):
-    ds = tf.data.Dataset.from_tensor_slices([random_text()])
-    ds = ds.repeat()
-    def sampler(x):
-      x = sample_from(x, params.n_ctx + 1)
-      x = tf.squeeze(x, 0)
-      x = tf.expand_dims(x, -1)
-      return x[0:-1], x[1:]
-    ds = ds.map(sampler, num_parallel_calls=params.num_parallel_calls)
-    ds = ds.batch(sub_batch_size)
-    def set_shape(*args):
-      for x in args:
-        shape = shapeof(x)
-        assert shape[0] is None or shape[0] == sub_batch_size
-        shape[0] = sub_batch_size
-        x.set_shape(shape)
-      return args
-    ds = ds.map(set_shape, num_parallel_calls=params.num_parallel_calls)
-    return ds
-
-def gpt_neo_input(params, sub_batch_size, slice_index, slice_count):
-    """
-    Input fn that reads tfrecords encoded with a fixed chunk size (== n_ctx + 1), and that either:
-
-        - has the number of documents for each tfrecord file encoded in the title in the format
-          <name>_<n_documents>.tfrecords.
-
-          OR
-
-        - has a fixed number of documents per tfrecord file.
-
-    If the glob pattern above isn't matched, we assume that each document has the same number of samples as the first
-    tfrecord read.
-    If this isn't the case, it may result in errors, or some samples being missed.
-
-    This means we can calculate the number of samples we've seen so far using the global step,
-    and can use dataset.skip() to iterate through the list of filenames, as opposed to the whole dataset, which is
-    incredibly inefficient.
-
-    If training is starting and stopping often, as with TPU pre-emption, reading the whole dataset sequentially appears
-    to improve model
-    performance, as it results in less repeated data.
-    :param params: serialized dict of ModelParameter instance
-    :return: tensorflow dataset
-    """
-
-    params = ModelParameter(params)
-    self = params
-
-    filenames = []
-    for config in params.dataset_configs:
-        filenames.extend(split_files(config['path'], slice_index, 1024,
-                                     params.shuffle_input_filenames * params.data_seed))
-
-    # repeat filenames to infinity
-    filenames_ds: Dataset = tf.data.Dataset.from_tensor_slices(filenames)
-
-    if self.is_training and not self.cache:
-      filenames_ds = filenames_ds.repeat()
-
-    def fetch_dataset(filename):
-      buffer_size = 8 * 1024 * 1024  # 8 MiB per file
-      dataset = tf.data.TFRecordDataset(filename, buffer_size=buffer_size)
-      return dataset
-
-    # Read the data from disk in parallel
-    tfrecorddataset = filenames_ds.apply(
-        tf_contrib.data.parallel_interleave(
-            fetch_dataset, cycle_length=self.num_parallel_readers, sloppy=True))
-    dataset = tfrecorddataset
-
-    # def chunk(data):
-    #     if chunk_size > 0:
-    #         data = data.batch(chunk_size)
-    #     data = data.window(size=ctx + patch_size, shift=ctx, stride=1, drop_remainder=True)
-    #     data = data.interleave(lambda x: x.batch(ctx + patch_size, drop_remainder=True))
-    #     return data
-    chunk = partial(ds_chunkify, chunk_size=params.chunk_size)
-    sample = partial(sample_text, amount=params.n_ctx + params.token_patch_size, maxlen=params.chunk_size)
-
-    dataset_parser_static = decode_intstring_static if 'int64' in filenames[0] else decode_bytestring_static
-
-    def _memory_func(x):
-        # x = tf.reshape(x, (params.n_ctx // params.token_patch_size + 1, params.token_patch_size))
-        # x = tf.cast(x, tf.int32)
-        vals1 = x[..., :-1]
-        vals2 = x[..., 1:]
-        # TODO: Why is tf.newaxis necessary here?
-        vals1 = vals1[..., tf.newaxis]
-        vals2 = vals2[..., tf.newaxis]
-        return {'token_x': vals1, 'token_y': vals2}
-
-    def dataset_parser_dynamic(x):
-      x = sample(x)
-      x = _memory_func(x)
-      x = align_tensor_op(x)
-      return x
-
-    def dataset_parser(x):
-      x = dataset_parser_static(x)
-      raise NotImplementedError("TODO: windowing")
-      x = dataset_parser_dynamic(x)
-      return x
-
-    # tfrecorddataset.map(dataset_parser_static).unbatch().window(size=params.n_ctx + params.token_patch_size, shift=params.n_ctx, stride=1, drop_remainder=True).batch(params.n_ctx + params.token_patch_size, drop_remainder=True)
-
-    if self.is_training and self.cache_decoded:
-      dataset = dataset.map(
-          dataset_parser_static,
-          num_parallel_calls=self.num_parallel_calls)
-      dataset = dataset.apply(chunk)
-        
-    if self.cache:
-      dataset = dataset.cache()
-    
-    if self.is_training:
-      # We shuffle only during training, and during training, we must produce an
-      # infinite dataset, so apply the fused shuffle_and_repeat optimized
-      # dataset transformation.
-      dataset = dataset.apply(
-          tf_contrib.data.shuffle_and_repeat(params.shuffle_buffer)) #(1024 * 16))
-
-    if not self.is_training:
-      # Padding for eval.
-      dataset = self.pad_dataset(dataset, num_hosts)
-
-
-    # Use the fused map-and-batch operation.
-    #
-    # For XLA, we must used fixed shapes. Because we repeat the source training
-    # dataset indefinitely, we can use `drop_remainder=True` to get fixed-size
-    # batches without dropping any training examples.
-    #
-    # When evaluating, `drop_remainder=True` prevents accidentally evaluating
-    # the same image twice by dropping the final batch if it is less than a full
-    # batch size. As long as this validation is done with consistent batch size,
-    # exactly the same images will be used.
-    if self.is_training and self.cache_decoded:
-      dataset = dataset.apply(
-          tf_contrib.data.map_and_batch(
-              dataset_parser_dynamic,
-              batch_size=sub_batch_size,
-              num_parallel_batches=self.num_cores,
-              drop_remainder=True))
-    else:
-      dataset = dataset.apply(
-          tf_contrib.data.map_and_batch(
-              dataset_parser,
-              batch_size=sub_batch_size,
-              num_parallel_batches=self.num_cores,
-              drop_remainder=True))
-
-    # Assign static batch size dimension
-    #dataset = dataset.map(partial(set_shapes, sub_batch_size))
-    #import pdb; pdb.set_trace()
-
-    # Prefetch overlaps in-feed with training
-    if self.prefetch_depth_auto_tune:
-      dataset = dataset.prefetch(tf_contrib.data.AUTOTUNE)
-    else:
-      dataset = dataset.prefetch(4)
-
     options = tf.data.Options()
+    options.experimental_optimization.autotune = True
+    options.experimental_optimization.autotune_buffers = True
+    options.experimental_optimization.filter_fusion = True
+    options.experimental_optimization.hoist_random_uniform = True
+    options.experimental_optimization.map_and_batch_fusion = True
+    options.experimental_optimization.map_and_filter_fusion = False
+    options.experimental_optimization.map_fusion = True
+    options.experimental_optimization.map_parallelization = True
+    options.experimental_optimization.map_vectorization.enabled = True
+    options.experimental_optimization.map_vectorization.use_choose_fastest = True
+    options.experimental_optimization.noop_elimination = True
+    options.experimental_optimization.parallel_batch = True
+    options.experimental_optimization.shuffle_and_repeat_fusion = True
+    options.experimental_optimization.apply_default_optimizations = False
     options.experimental_threading.max_intra_op_parallelism = 1
     options.experimental_threading.private_threadpool_size = 48
-    dataset = dataset.with_options(options)
-    return dataset
-
-    
-
-def foo():
-    def _memory_func(x):
-        x = tf.reshape(x, (sub_batch_size, params.n_ctx // params.token_patch_size + 1, params.token_patch_size))
-        x = tf.cast(x, tf.int32)
-
-        vals1 = x[:, :params.n_ctx]
-        vals2 = x[:, 1:params.n_ctx + 1]
-
-        return {'token_x': vals1, 'token_y': vals2}
-
-    dset = dset.interleave(lambda x: _text_decoder(decoder, x, params.n_ctx, params.token_patch_size, -1))
-
-    # dset = dset.shuffle(params.shuffle_buffer, seed=params.data_seed)
-    dset = dset.batch(sub_batch_size)
-
-    dset = dset.map(_memory_func)
-    dset = dset.map(align_tensor_op)
+    options.experimental_distribute.auto_shard_policy = 1  # -1=Off, 0=Auto, 1=File, 2=Data
+    dset = dset.with_options(options)
 
     return dset
