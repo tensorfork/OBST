04a1670fd8951bc93ff33e434fd15b3fd1454f8a
ClashLuke <39779310+clashluke@users.noreply.github.com>
2021-03-22 14:42:17 +0100

    fix(model): use correct indendation, correctly implement pcgrad, fix type annotations

--- src/optimizers.py
+++ src/optimizers.py
@@ -22,13 +22,14 @@ def sum_dim(inp: mtf.Tensor, dims: typing.List[mtf.Dimension]):
     return mtf.einsum([inp, anonymize(dims, dims)], output_shape=mtf.Shape(dims) + [anonymize_dim(d) for d in dims])
 
 
-def get_optimizer(loss_list: mtf.Tensor, params: ModelParameter, manual_step
-                  ) -> typing.Tuple[typing.Tuple[mtf.Tensor, typing.List[mtf.Assign],
-                                                 typing.List[mtf.Tensor]], tf.Tensor]:
+def get_optimizer(loss_list: typing.List[mtf.Tensor], params: ModelParameter, manual_step: tf.Tensor,
+                  ) -> typing.Tuple[typing.Tuple[mtf.Tensor, typing.List[mtf.Assign], typing.List[mtf.Tensor]],
+                                    tf.Tensor]:
     """
     Creates optimizing and update/training operations.
-    :param loss: Final scalar loss of the model
+    :param loss_list: Final scalar loss of the model
     :param params: ModelParameter instance
+    :param manual_step: manually incremented global_step variable to account for grad accumulation
     :return: scalar learning rate, update operations, gradients
     """
 
@@ -65,10 +66,10 @@ def get_optimizer(loss_list: mtf.Tensor, params: ModelParameter, manual_step
         return mtf.get_variable(var.mesh, f"{var.name}/{params.optimizer}/{name}", shape,
                                 initializer=tf.zeros_initializer(), trainable=False, dtype=params.variable_dtype)
 
-    shared_operations = loss_list[0].graph.operations
+    shared_operations = set(loss_list[0].graph.operations)
 
     for loss_idx in range(1, len(loss_list)):
-        shared_operations = list(set(shared_operations) & set(loss_list[loss_idx].graph.operations))
+        shared_operations &= set(loss_list[loss_idx].graph.operations)
 
     for loss_idx, loss in enumerate(loss_list):
 
@@ -128,24 +129,19 @@ def get_optimizer(loss_list: mtf.Tensor, params: ModelParameter, manual_step
 
                         if params.use_PCGrad and len(loss_list) > 1:
                             if op in shared_operations:
-                                flatt_grad_dim = mtf.Dimension("flatt_grad", size=grad.size)
-                                grad_flat_shape = mtf.Shape([flatt_grad_dim])
-                                grad_flat = mtf.reshape(grad, grad_flat_shape)
-                                second_grad = variable(var, "second_grad", grad_flat.shape)
-
-                                if (loss_idx + 1) < len(loss_list):
-                                    update_ops.append(mtf.assign(second_grad, grad_flat))
+                                other_grads = [variable(var, f"grad_{i}", var.shape) for i in range(len(loss_list) - 1)]
+                                if loss_idx < len(loss_list) - 1:
+                                    update_ops.append(mtf.assign(other_grads[loss_idx], grad))
                                     continue
-
-                                else:
-                                    inner_product = mtf.reduce_sum((grad_flat * second_grad),
-                                                                   reduced_dim=flatt_grad_dim)
-                                    proj_direction = inner_product / mtf.reduce_sum((second_grad * second_grad),
-                                                                                    reduced_dim=flatt_grad_dim)
-                                    grad_flat = grad_flat - mtf.minimum(proj_direction, 0.0) * second_grad
-
-                                    grad = mtf.reshape(grad_flat, new_shape=var.shape)
-
+                                other_grads.insert(0, grad)
+                                g_square = [mtf.einsum([g, g], output_shape=[])]
+                                for i in range(len(other_grads)):
+                                    grad = other_grads.pop(0)
+                                    for g, sq in zip(other_grads, g_square):
+                                        grad -= g * (mtf.minimum(mtf.einsum([grad, g], output_shape=[]), 0) / sq)
+                                    other_grads.append(grad)
+                                    g_square.append(mtf.einsum([g, g], output_shape=[]))
+                                grad = mtf.add_n(other_grads)
 
                         if params.grad_accumulation > 1:
                             grad_buffer = variable(var, "grad_accumulation", var.shape)
@@ -190,6 +186,7 @@ def get_optimizer(loss_list: mtf.Tensor, params: ModelParameter, manual_step
                             update_ops.extend([mtf.assign(exp_avg_p1_ptr, beta1 * exp_avg_p1_ptr +
                                                           grad * mtf.rsqrt(exp_avg_p2 + epsilon)),
                                                mtf.assign(exp_avg_p2_ptr, exp_avg_p2)])
+
                         elif params.optimizer == 'sm3':
                             update = variable(var, "dim0", [var.shape.dims[0]])
                             buffer = [update]
@@ -200,34 +197,34 @@ def get_optimizer(loss_list: mtf.Tensor, params: ModelParameter, manual_step
 
                             update += mtf.square(grad)
 
-                        weight_update = grad * mtf.rsqrt(update + epsilon)
-                        update_ops.extend([mtf.assign(buf_ptr, mtf.reduce_max(update, output_shape=[dim]))
-                                           for buf_ptr, dim in zip(buffer, update.shape.dims)])
-                    weight_update *= learning_rate
-                    if params.weight_decay > 0:
-                        weight_update += params.weight_decay * var.value
-                    if var.shape.size > 1:
-                        weight_update += mtf.reduce_mean(var.value)
-                    if params.grad_accumulation > 1:
-                        weight_update *= step
-                    feature_dims_used = all(f.size in var.shape for f in params.feature_dims)
-                    if params.weight_standardisation and \
-                            ((feature_dims_used and len(var.shape) > len(params.feature_dims))
-                             or (not feature_dims_used and len(var.shape) >= 2)):
-                        val = var.value
-                        val -= weight_update
-                        std = mtf.rsqrt(1e-6 + mtf.reduce_mean(mtf.square(val), output_shape=[]))
+                            weight_update = grad * mtf.rsqrt(update + epsilon)
+                            update_ops.extend([mtf.assign(buf_ptr, mtf.reduce_max(update, output_shape=[dim]))
+                                               for buf_ptr, dim in zip(buffer, update.shape.dims)])
 
-                        shape = [d.size for d in var.shape.dims]
-                        if feature_dims_used and shape.index(params.key_dim.size) == -1:
-                            fan_in = np.prod(shape[:-2])
-                        elif feature_dims_used:
+                        weight_update *= learning_rate
+                        if params.weight_decay > 0:
+                            weight_update += params.weight_decay * var.value
+                        if var.shape.size > 1:
+                            weight_update += mtf.reduce_mean(var.value)
+                        if params.grad_accumulation > 1:
+                            weight_update *= step
+                        feature_dims_used = all(f.size in var.shape for f in params.feature_dims)
+                        if (params.weight_standardisation and
+                                ((feature_dims_used and len(var.shape) > len(params.feature_dims))
+                                 or (not feature_dims_used and len(var.shape) >= 2))):
+                            val = var.value
+                            val -= weight_update
+                            std = mtf.rsqrt(1e-6 + mtf.reduce_mean(mtf.square(val), output_shape=[]))
 
-                            fan_in = np.prod(shape[:2])
+                            shape = [d.size for d in var.shape.dims]
+                            if feature_dims_used and shape.index(params.key_dim.size) == -1:
+                                fan_in = np.prod(shape[:-2])
+                            elif feature_dims_used:
+                                fan_in = np.prod(shape[:2])
+                            else:
+                                fan_in = shape[0]
+                            std *= 1.6077447771479307 / np.sqrt(fan_in)
+                            update_ops.append(mtf.assign(var, val * std))
                         else:
-                            fan_in = shape[0]
-                        std *= 1.6077447771479307 / np.sqrt(fan_in)
-                        update_ops.append(mtf.assign(var, val * std))
-                    else:
-                        update_ops.append(mtf.assign_sub(var, weight_update))
+                            update_ops.append(mtf.assign_sub(var, weight_update))
     return params.mesh.graph.trainable_variables[0].graph.combine_assignments(update_ops), tf_learning_rate
