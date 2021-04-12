fix(model.py): Information leek in SoftMax.

src/model.py
@@ -138,7 +138,7 @@ class ConvolutionFilterBackward(mtf.Operation):
 def tf_softmax(x, masked, dim, dim_index, anonymous_dim_index):
     if masked:
         arange = tf.range(0, dim.size)
-        msk = tf.reshape(arange, (1, dim.size)) > tf.reshape(arange, (dim.size, 1))
+        msk = tf.reshape(arange, (1, dim.size)) < tf.reshape(arange, (dim.size, 1))
         msk = tf.cast(msk, x.dtype)
         msk *= 1e12
         shape = [1] * len(x.shape)