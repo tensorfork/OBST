fix(model): softmax over the correct dim + mask -inf

src/model.py
@@ -140,14 +140,14 @@ def tf_softmax(x, masked, dim, dim_index, anonymous_dim_index):
         arange = tf.range(0, dim.size)
         msk = tf.reshape(arange, (1, dim.size)) > tf.reshape(arange, (dim.size, 1))
         msk = tf.cast(msk, x.dtype)
-        msk *= 1e12
+        msk *= 1e120
         shape = [1] * len(x.shape)
         shape[dim_index] = dim.size
         shape[anonymous_dim_index] = dim.size
         msk = tf.reshape(msk, shape)
         x -= msk
-    e = tf.exp(x - tf.reduce_max(x, dim_index, True))
-    return e / tf.reduce_sum(e, dim_index, True)
+    e = tf.exp(x - tf.reduce_max(x, anonymous_dim_index, True))
+    return e / tf.reduce_sum(e, anonymous_dim_index, True)
 
 
 class SoftmaxBackward(mtf.Operation):