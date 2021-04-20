Fix gradient accumulation
- When manual_step + 1 mod grad_accumulation == 0 (i.e. when the
  gradient step is just about to wrap around)...

  1. set the grad accum buffer to zero

  2. use (grad + grad_buffer) as the gradient for the current step

  3. increment global_step

This ensures the following invariants:

- global_step will increment only after the gradient accumulators are
  applied. (Previously, global_step was incorrectly incremented when
  manual_step == 0.)

- when manual_step mod grad_accumulation == 0, all gradient accumulators
  are zero

- all gradients are seen, and every gradient is seen exactly once

src/optimizers.py
@@ -65,7 +65,7 @@ def get_optimizer(loss_list: typing.List[mtf.Tensor], params: ModelParameter, ma
                                       import_float(params.learning_rate_decay_min * 1.))
 
     learning_rate = import_mtf(tf_learning_rate, "learning_rate")
-    step = cast(equal(mod(tf.cast(manual_step, dtype),
+    step = cast(equal(mod(tf.cast(manual_step + 1, dtype),
                           import_mtf(params.grad_accumulation * 1., "grad_accum")),
                       import_mtf(0., "zero")), dtype)
     mstep = 1 - step
@@ -211,8 +211,9 @@ def get_optimizer(loss_list: typing.List[mtf.Tensor], params: ModelParameter, ma
 
                         if params.grad_accumulation > 1:
                             grad_buffer = variable(var, "grad_accumulation", var.shape)
-                            update_ops.append(mtf.assign(grad_buffer, grad + grad_buffer * mstep))
-                            grad = grad_buffer * step / params.grad_accumulation
+                            next_grad = grad + mtf.identity(grad_buffer)
+                            update_ops.append(mtf.assign(grad_buffer, next_grad * mstep))
+                            grad = next_grad * step / params.grad_accumulation
 
                         features_used = feature_dims_used(params, var)
                         if features_used and var.shape.dims.index(params.key_dim) == var.shape.ndims - 1: