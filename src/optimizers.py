5af86c19e4832de740ddfaf33c25d1433c211c55
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-04-21 13:06:33 +0200

    fix(optimizer): add support for fan_in of scalars

--- src/optimizers.py
+++ src/optimizers.py
@@ -219,8 +219,10 @@ def get_optimizer(loss_list: typing.List[mtf.Tensor], params: ModelParameter, ma
                             fan_in = var.shape.dims[:-2]
                         elif features_used:
                             fan_in = var.shape.dims[:2]
-                        else:
+                        elif var.shape.ndims > 0:
                             fan_in = var.shape.dims[0]
+                        else:
+                            fan_in = 1
                         if params.gradient_clip > 0 and params.adaptive_gradient_clipping:
                             grd_norm = sqrt(einsum([grad, grad], reduced_dims=fan_in) + 1e-5)
                             wgt_norm = sqrt(einsum([var.value, var.value], reduced_dims=fan_in) + 1e-3)
