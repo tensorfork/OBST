fix(model): remove stop_gradient

src/eval.py
@@ -60,7 +60,6 @@ def render_video(model_output: typing.List[typing.Tuple[np.ndarray, typing.List[
 
 def process_token_output(token_out: np.ndarray, padding_token: int = -1, do_argmax: bool = True,
                          bpe_tokenizer: GPT2TokenizerFast = None) -> typing.List[str]:
-
     _shape = token_out.shape
     if do_argmax:
         token_out = np.reshape(token_out, newshape=(_shape[0], _shape[1] * _shape[2], _shape[3]))
@@ -76,11 +75,11 @@ def process_token_output(token_out: np.ndarray, padding_token: int = -1, do_argm
 
         if bpe_tokenizer is None:
             token_out_str.append(
-                "".join(
-                    chr(tok) if tok > 31 and tok != 127 and tok != 10 else " "
-                    for tok in token
-                )
-            )
+                    "".join(
+                            chr(tok) if tok > 31 and tok != 127 and tok != 10 else " "
+                            for tok in token
+                            )
+                    )
 
         else:
             token_out_str.append(bpe_tokenizer.decode([int(tok) for tok in token]))
@@ -138,7 +137,7 @@ def gen_sample_fn(params: ModelParameter):
     def _text_fn(out):
         print('sample_idx:', state['sample_index'])
 
-        bpe_tokenizer = GPT2TokenizerFast.from_pretrained('gpt2') if params.vocab_size != 256 and\
+        bpe_tokenizer = GPT2TokenizerFast.from_pretrained('gpt2') if params.vocab_size != 256 and \
                                                                      params.vocab_size > 256 else None
 
         if params.use_autoregressive_sampling:
@@ -154,9 +153,11 @@ def gen_sample_fn(params: ModelParameter):
             print('\n------\n')
             color_print(params, 'Prompt:')
             assert params.initial_autoregressive_position > 0
-            print(process_token_output(out[1][:, :params.initial_autoregressive_position-1], do_argmax=False, bpe_tokenizer=bpe_tokenizer)[0])
+            print(process_token_output(out[1][:, :params.initial_autoregressive_position - 1], do_argmax=False,
+                                       bpe_tokenizer=bpe_tokenizer)[0])
             color_print(params, 'Output:')
-            print(process_token_output(out[0][:, params.initial_autoregressive_position:], do_argmax=False, bpe_tokenizer=bpe_tokenizer)[0].rstrip())
+            print(process_token_output(out[0][:, params.initial_autoregressive_position:], do_argmax=False,
+                                       bpe_tokenizer=bpe_tokenizer)[0].rstrip())
         else:
             print('target:')
             print(process_token_output(out[1], do_argmax=False, bpe_tokenizer=bpe_tokenizer)[0])

src/model/__init__.py
@@ -9,7 +9,7 @@ from .revnet import RevGradOp
 from ..dataclass import BlockConfig, ModelParameter
 from ..mtf_wrapper import (add_n, cast, constant_scalar, dropout, einsum, exp, log, one_hot, ones, reciprocal,
                            reduce_logsumexp, reduce_max, reduce_sum, sigmoid, sign, zeros_like)
-from ..utils_mtf import concat, guarantee_const, head_argmax, head_embed, slice
+from ..utils_mtf import concat, head_argmax, head_embed, slice
 
 ATTENTION_DIM = typing.NamedTuple("AttentionDim", (('index', int), ('dim', mtf.Dimension)))
 
@@ -18,8 +18,8 @@ tf1 = tf.compat.v1
 
 def _default_ones(params: ModelParameter, inp: typing.Optional[mtf.Tensor]) -> mtf.Tensor:
     if inp is None:
-        return guarantee_const(ones(params.mesh, [], params.variable_dtype.activation_dtype))
-    return cast(mtf.stop_gradient(inp), params.variable_dtype.activation_dtype)
+        return ones(params.mesh, [], params.variable_dtype.activation_dtype)
+    return cast(inp, params.variable_dtype.activation_dtype)
 
 
 def build(params: ModelParameter,
@@ -65,7 +65,7 @@ def build(params: ModelParameter,
         spatial_ctx: mtf.Dimension = txt_tgt.shape[-2] if params.use_language else vid.shape[2]
 
         if params.use_video:
-            vid = mtf.stop_gradient(vid)
+
             vid = dropout(vid, rate=params.input_dropout)
 
             if params.use_bit_fold_input_pipeline:
@@ -83,8 +83,6 @@ def build(params: ModelParameter,
 
             if not params.use_discrete_video_loss:
                 vid = mtf.cast(vid, params.variable_dtype.activation_dtype) / 255
-            vid = mtf.stop_gradient(vid)
-
             context_dimension = vid.shape[1]
             input_features = vid.shape[-1:]
             tgt = slice(vid, 1, context_dimension.size, context_dimension)
@@ -97,7 +95,6 @@ def build(params: ModelParameter,
                                                             params.sequence_per_head_dim,
                                                             params.head_dim]
                                                            + tgt.shape[2:]))
-            src = mtf.stop_gradient(src)
 
             src = src * vid_msk_src + embed(params, shape=vid.shape[2:]) * (1 - vid_msk_src)
             src = src * cat_msk_src + embed(params, shape=vid.shape[2:]) * (1 - cat_msk_src)
@@ -109,8 +106,6 @@ def build(params: ModelParameter,
 
         # Language embedding and initial feed forward.
         if params.use_language:
-            txt_tgt = mtf.stop_gradient(txt_tgt)
-            txt_src = mtf.stop_gradient(txt_src)
             txt = einsum([embed(params, [params.head_dim, params.vocab_dim] + params.intermediate),
                           *head_embed(params, txt_src)], reduced_dims=[params.vocab_dim, params.head_dim])
 
@@ -189,8 +184,8 @@ def build(params: ModelParameter,
 
         if params.use_language:
             reduced_shape = token_out.shape - params.vocab_dims
-            max_logit = reduce_max(mtf.stop_gradient(token_out), output_shape=reduced_shape)
-            msk = mtf.stop_gradient(txt_msk * cat_msk_tgt * (1 / txt_tgt.size))
+            max_logit = reduce_max(token_out, output_shape=reduced_shape)
+            msk = txt_msk * cat_msk_tgt * (1 / txt_tgt.size)
             token_loss = einsum([log(reduce_sum(exp(token_out - max_logit), output_shape=reduced_shape)), msk],
                                 output_shape=[])
             token_loss += einsum([token_out, *head_embed(params, txt_tgt), constant_scalar(params, -1), msk],
@@ -201,14 +196,11 @@ def build(params: ModelParameter,
             if txt_msk is not None:
                 token_loss = einsum([constant_scalar(params, txt_msk.size), reciprocal(reduce_sum(txt_msk)),
                                      constant_scalar(params, cat_msk_tgt.size), reciprocal(reduce_sum(cat_msk_tgt)),
-                                     mtf.stop_gradient(token_loss)], output_shape=[])
-            token_loss = mtf.stop_gradient(token_loss)
+                                     token_loss], output_shape=[])
 
             if params.calc_accuracy:
-                accuracy = einsum([cast(mtf.equal(head_argmax(mtf.stop_gradient(token_out), params.vocab_dims),
-                                                  txt_tgt),
+                accuracy = einsum([cast(mtf.equal(head_argmax(token_out, params.vocab_dims), txt_tgt),
                                         params.variable_dtype.activation_dtype), msk], output_shape=[])
-                accuracy = mtf.stop_gradient(accuracy)
 
         if params.use_video:
 
@@ -228,7 +220,7 @@ def build(params: ModelParameter,
 
             else:
                 size = constant_scalar(params, 1 / frame_out.size)
-                out = frame_out - mtf.stop_gradient(tgt)
+                out = frame_out - tgt
                 video_loss: mtf.Tensor = einsum([out, vid_msk_tgt, cat_msk_tgt, size, sign(out)], output_shape=[])
 
             loss_list.append(video_loss)
@@ -236,8 +228,7 @@ def build(params: ModelParameter,
             if vid_msk_tgt is not None:
                 video_loss = einsum([constant_scalar(params, vid_msk_tgt.size), reciprocal(reduce_sum(vid_msk_tgt)),
                                      constant_scalar(params, cat_msk_tgt.size), reciprocal(reduce_sum(cat_msk_tgt)),
-                                     mtf.stop_gradient(video_loss)], output_shape=[])
-            video_loss = mtf.stop_gradient(video_loss)
+                                     video_loss], output_shape=[])
         params.layer_idx = 0
 
         return add_n(loss_list), loss_list, video_loss, accuracy, token_loss, frame_out, token_out

src/model/basic.py
@@ -11,7 +11,7 @@ from ..dataclass import ModelParameter
 from ..mtf_wrapper import (add_n, constant_scalar, dropout as utils_dropout, einsum, exp, mod, mtf_range, reduce_mean,
                            rsqrt, sigmoid, sin)
 from ..utils_core import random_name
-from ..utils_mtf import DIM_LIST, SHAPE, anonymize_dim, dims_from_shape, guarantee_const, shape_size
+from ..utils_mtf import DIM_LIST, SHAPE, anonymize_dim, dims_from_shape, shape_size
 
 ATTENTION_DIM = typing.NamedTuple("AttentionDim", (('index', int), ('dim', mtf.Dimension)))
 
@@ -128,8 +128,8 @@ def rezero(params, block_input: mtf.Tensor, name_extras: typing.List[str]) -> mt
 
 
 def _multi_dim_range(params: ModelParameter, dims: DIM_LIST) -> mtf.Tensor:
-    return guarantee_const(add_n([mtf_range(params.mesh, dim, params.variable_dtype.activation_dtype) * size
-                                  for dim, size in zip(dims, np.cumprod([1] + [d.size for d in dims[:-1]]))]))
+    return add_n([mtf_range(params.mesh, dim, params.variable_dtype.activation_dtype) * size
+                  for dim, size in zip(dims, np.cumprod([1] + [d.size for d in dims[:-1]]))])
 
 
 _EMBEDDINGS = {}
@@ -180,7 +180,7 @@ def embed(params: ModelParameter, shape: SHAPE) -> mtf.Tensor:
             feature_count /= 2
         features -= math.log(math.pi * 2 / position_count) - feature_count / 2
         features *= feature_count ** -0.5
-        out = guarantee_const(sin(op(positions, exp(features) + additive)) * params.embedding_stddev)
+        out = sin(op(positions, exp(features) + additive)) * params.embedding_stddev
         if learned:
             out *= normal_var(params, feature_dims, 1)
     if axial:

src/utils_mtf.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 from .dataclass import ModelParameter
 from .mtf_wrapper import floordiv, mod, one_hot
-from .utils_core import default, random_name
+from .utils_core import default
 
 tf1 = tf.compat.v1
 
@@ -17,26 +17,6 @@ TENSORS = typing.List[mtf.Tensor]
 OPT_SHAPE = typing.Optional[SHAPE]
 OPT_DIMS = typing.Optional[DIM_LIST]
 ALL_SHAPES = typing.Union[SHAPE, mtf.Tensor, mtf.Variable]
-
-
-class GuaranteeConst(mtf.Operation):
-    """
-    A tensor that always is constant.
-    It acts as a hint to the compiler to optimize a bit better.
-    """
-
-    def __init__(self, value: mtf.Tensor):
-        super().__init__([value], name=random_name("guarantee_const"))
-        self._outputs = [mtf.Tensor(self, value.shape, value.dtype)]
-
-    def lower(self, lowering):
-        mesh_impl = lowering.mesh_impl(self)
-        y = mesh_impl.slicewise(lambda x: tf.guarantee_const(tf.stop_gradient(x)), lowering.tensors[self.inputs[0]])
-        lowering.set_tensor_lowering(self.outputs[0], y)
-
-
-def guarantee_const(value):
-    return GuaranteeConst(value).outputs[0]
 
 
 def head_argmax(tensor: mtf.Tensor, dims: typing.List[mtf.Dimension]) -> mtf.Tensor:
@@ -57,6 +37,8 @@ def head_embed(params: ModelParameter, int_tokens: mtf.Tensor) -> typing.Tuple[m
                     dtype=params.variable_dtype.activation_dtype),
             one_hot(mod(int_tokens, params.vocab_size), params.vocab_dim,
                     dtype=params.variable_dtype.activation_dtype))
+
+
 def unanonymize(inp: mtf.Tensor, dim: typing.Union[mtf.Dimension, str]) -> mtf.Tensor:
     """
     Inverse of anonymize. Un-replicates tensor across axis by removing the underscore from the name of a dimension of