1955a28925fc4419d801abb34bf58ec7e6afa2eb
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-05-19 01:50:06 +0200

    feat(model): add spatial_mixing

--- src/model/basic.py
+++ src/model/basic.py
@@ -4,9 +4,12 @@ import mesh_tensorflow as mtf
 import tensorflow as tf
 
 from .activation import activate_util
-from .backend import get_intermediate, get_variable, linear_from_features, linear_to_features
+from .backend import (get_attention_dim, get_intermediate, get_variable, linear_from_features, linear_to_features,
+                      orthogonal_var)
+from .normalization import norm
 from ..dataclass import ModelParameter
-from ..mtf_wrapper import (dropout as utils_dropout, sigmoid)
+from ..mtf_wrapper import dropout as utils_dropout, einsum, sigmoid
+from ..utils_mtf import anonymize, anonymize_dim, deduplicate
 
 ATTENTION_DIM = typing.NamedTuple("AttentionDim", (('index', int), ('dim', mtf.Dimension)))
 
@@ -25,15 +28,42 @@ def dropout(params: ModelParameter, block_input: mtf.Tensor, name_extras: typing
     return utils_dropout(block_input, keep)
 
 
-def feed_forward(params: ModelParameter, block_input: mtf.Tensor, name_extras: typing.List[str]) -> mtf.Tensor:
-    intermediate = get_intermediate(params, name_extras)
-
+def feed_forward_in(params: ModelParameter, block_input: mtf.Tensor, name_extras: typing.List):
     def _from_feat():
-        return linear_from_features(params, block_input, intermediate)
+        return linear_from_features(params, block_input, get_intermediate(params, name_extras))
 
-    mid = dropout(params, activate_util(name_extras, _from_feat()), name_extras)
+    out = dropout(params, activate_util(name_extras, _from_feat()), name_extras)
     if 'glu' in name_extras or 'glu_add' in name_extras:
-        mid *= sigmoid(_from_feat())
+        out *= sigmoid(_from_feat())
     if 'glu_add' in name_extras:
-        mid += activate_util(name_extras, _from_feat())
-    return linear_to_features(params, mid, intermediate)
+        out += activate_util(name_extras, _from_feat())
+    return out
+
+
+def feed_forward_out(params: ModelParameter, block_input: mtf.Tensor, name_extras: typing.List):
+    return linear_to_features(params, block_input, get_intermediate(params, name_extras))
+
+
+def feed_forward(params: ModelParameter, block_input: mtf.Tensor, name_extras: typing.List[str]) -> mtf.Tensor:
+    return feed_forward_out(params, feed_forward_in(params, block_input, name_extras), name_extras)
+
+
+def spatial_mixing(params: ModelParameter, block_input: mtf.Tensor, name_extras: typing.List[str]) -> mtf.Tensor:
+    dim = get_attention_dim(params, block_input)
+    tmp = anonymize_dim(dim)
+
+    if 'feed_forward' in name_extras:
+        mid = feed_forward_in(params, block_input, name_extras)
+    else:
+        mid = block_input
+
+    if 'norm' in name_extras:
+        mid = norm(params, block_input, name_extras + ['group'])
+    mid = anonymize(mid, dim)
+    old = [params.head_dim, tmp]
+    new = [params.head_dim, dim]
+    mid = einsum([mid, block_input, orthogonal_var(params, old + new)],
+                 deduplicate((block_input.shape - old).dims + new))
+    if 'feed_forward' not in name_extras:
+        return mid
+    return feed_forward_out(params, block_input, name_extras)
