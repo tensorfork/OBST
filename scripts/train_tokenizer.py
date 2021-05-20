ff83decd5189ca1399092439ef39a00d94674451
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-05-20 06:55:33 +0200

    feat(tokenizer): add support for pretty console prints

--- scripts/train_tokenizer.py
+++ scripts/train_tokenizer.py
@@ -21,21 +21,19 @@ from tokenizers.pre_tokenizers import Split
 from tokenizers.trainers import BpeTrainer
 
 
-def file_generator(queue: Queue, lock: threading.Semaphore, pid, procs):
+def file_generator(queue: Queue, lock: threading.Semaphore, pid: int, procs: int, base_path: str):
     base_url = 'http://eaidata.bmk.sh/data/pile/train/%s.jsonl.zst'
     splits = 30
     parse_fn = simdjson.Parser().parse
-    tmp_name = f"E:\\Pile\\download.{pid}.zstd"
+    tmp_name = f"{base_path}download\\{pid}.zstd"
+    log_path = f"{base_path}log\\{pid}.txt"
     total = [0]
 
-    if not os.path.exists('pile'):
-        os.mkdir('pile')
+    with open(log_path, 'w') as f:
+        f.write('')
 
     def _parser(x):
-        try:
-            return parse_fn(x).as_dict()
-        except:
-            return x
+        return parse_fn(x.encode()).as_dict()
 
     def _write(text):
         out = ftfy.fix_text(text).replace('    ', '\t')
@@ -43,28 +41,31 @@ def file_generator(queue: Queue, lock: threading.Semaphore, pid, procs):
         return out
 
     for i in range(pid, splits, procs):
+        i = 2
+
+        def _log(text):
+            with open(log_path, 'a') as f:
+                f.write(f'Proc: {pid} | Slice: {i} | Time: {datetime.datetime.now()} | {text}\n')
+
         total[0] = 0
-        print(f"Starting {i} at {datetime.datetime.now()}")
+        _log(f"Starting")
         lock.acquire()
-        start = time.time()
-        print(f"Downloading {i}")
+        _log(f"Downloading")
         with requests.get(base_url.replace("%s", str(i).zfill(2)), stream=True) as r, open(tmp_name, 'wb') as f:
             shutil.copyfileobj(r.raw, f)
-        print(f"Finished downloading {i} after {time.time() - start:.1f}s")
+        _log(f"Finished downloading")
         lock.release()
-        start = time.time()
         with open(tmp_name, 'rb') as f:
             read = jsonlines.Reader(io.BufferedReader(zstandard.ZstdDecompressor().stream_reader(f)), loads=_parser)
             for idx, item in enumerate(read):
-                if isinstance(item, dict):
-                    item = item['text']
+                item = item['text']
                 if isinstance(item, list):
                     for itm in item:
                         queue.put(_write(itm))
                 else:
                     queue.put(_write(item))
                 if idx % 100000 == 0:
-                    print(f"Slice: {i} - Chars: {total[0]} - Took: {time.time() - start:.1f}s")
+                    _log(f"{total[0] / 2 ** 20:9.2f}MB")
         os.remove(tmp_name)
 
 
@@ -78,10 +79,16 @@ def main():
     parser.add_argument("--pile", type=bool, default=False, help="Whether to download the pile")
     parser.add_argument("--vocab_size", type=int, default=65536, help="Items in vocabulary")
     parser.add_argument("--procs", type=int, default=8, help="Number of processes")
+    parser.add_argument("--prefetch", type=int, default=128, help="Prefetch queue size")
+    parser.add_argument("--base_path", type=str, default='E:\\Pile\\', help="Path of data and logs")
     parser.add_argument("--separator", type=int, default=4, help="Separator character")
     parser.add_argument("--cache_capacity", type=int, default=1024 * 1024 * 1024,
                         help="Number of words to keep in BPE cache")
     args = parser.parse_args()
+
+    for path in ('', 'download', 'log'):
+        if not os.path.exists(args.base_path + path):
+            os.mkdir(args.base_path + path)
 
     backslash = "\\"
     chars = f'{string.digits} \t\n\r\x0b\x0c{"".join(f"{backslash}{c}" for c in string.punctuation)}'
@@ -95,10 +102,10 @@ def main():
 
     # train and save
     manger = multiprocessing.Manager()
-    queue = manger.Queue(16)
+    queue = manger.Queue(args.prefetch)
     lock = manger.Semaphore()
 
-    procs = [multiprocessing.Process(target=file_generator, args=(queue, lock, i, args.procs))
+    procs = [multiprocessing.Process(target=file_generator, args=(queue, lock, i, args.procs, args.base_path))
              for i in range(args.procs)]
     for p in procs:
         p.start()
@@ -111,7 +118,7 @@ def main():
                 if not any(p.is_alive() for p in procs):
                     return
 
-    while queue.qsize() < 4:
+    while queue.qsize() < args.prefetch // 2:
         time.sleep(5)
     tokenizer.train_from_iterator(_iterator(), trainer)
     for p in procs:
