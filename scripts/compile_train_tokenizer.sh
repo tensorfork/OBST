bfe10172e4c2f1cbf560af82bf9bdce84b01a508
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-06-01 03:57:57 +0200

    perf(scripts): add more compiler flags

--- scripts/compile_train_tokenizer.sh
+++ scripts/compile_train_tokenizer.sh
@@ -1,7 +1,7 @@
 function compile {
   file=${1}
   cython "$file.pyx"  -3 -Wextra -D
-  flags="$file.c `python3-config --cflags --ldflags --includes --libs` -I`python3 -c 'import numpy, sys; sys.stdout.write(numpy.get_include()); sys.stdout.flush()'` -fno-lto -pthread -fPIC -fwrapv -pipe -march=native -mtune=native -Ofast -msse2 -msse4.2 -shared -o $file.so"
+  flags="$file.c `python3-config --cflags --ldflags --includes --libs` -I`python3 -c 'import numpy, sys; sys.stdout.write(numpy.get_include()); sys.stdout.flush()'` -fsingle-precision-constant -fcx-fortran-rules -flto -pthread -fPIC -fwrapv -pipe -march=native -mtune=native -Ofast -msse2 -msse4.2 -shared -o $file.so"
   echo "Executing gcc with $flags"
   (gcc-9 $flags) || (gcc-8 $flags) || (gcc-7 $flags) || (gcc $flags)
   echo "Testing compilation.."
