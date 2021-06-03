db1153c1d10fecb1f4005544a895f19c9b2aaf49
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-06-03 05:35:32 +0200

    style(compiler): add gcc11

--- scripts/compile_train_tokenizer.sh
+++ scripts/compile_train_tokenizer.sh
@@ -3,7 +3,7 @@ function compile {
   cython "$file.pyx"  -3 -Wextra -D
   flags="$file.c `python3-config --cflags --ldflags --includes --libs` -I`python3 -c 'import numpy, sys; sys.stdout.write(numpy.get_include()); sys.stdout.flush()'` -fsingle-precision-constant -fcx-fortran-rules -flto -pthread -fPIC -fwrapv -pipe -march=native -mtune=native -Ofast -msse2 -msse4.2 -shared -o $file.so"
   echo "Executing gcc with $flags"
-  (gcc-9 $flags) || (gcc-8 $flags) || (gcc-7 $flags) || (gcc $flags)
+  (gcc-11 $flags) || (gcc-10 $flags) || (gcc-9 $flags) || (gcc-8 $flags) || (gcc-7 $flags) || (gcc $flags)
   echo "Testing compilation.."
   python3 -c "import $file"
   echo
