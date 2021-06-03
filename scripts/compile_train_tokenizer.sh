1e7599d4076533439bb2484759c570f1dc64e0fe
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-06-03 08:06:00 +0200

    perf(train_tokenizer): add gcc-11/gcc-10 support

--- scripts/compile_train_tokenizer.sh
+++ scripts/compile_train_tokenizer.sh
@@ -20,10 +20,11 @@ echo "$gcc_options"
 
 function compile {
   file=${1}
+  echo "Cythonizing.."
   cython "$file.pyx"  -3 -Wextra -D
   flags="$file.c $gcc_options -o $file.so"
-  echo "Executing gcc"
-  time ((gcc-9 $flags) || (gcc-8 $flags) || (gcc-7 $flags) || (gcc $flags))
+  echo "Executing gcc.."
+  time ((gcc-11 $flags) || (gcc-10 $flags) || (gcc-9 $flags) || (gcc-8 $flags) || (gcc-7 $flags) || (gcc $flags))
   echo "Testing compilation.."
   python3 -c "import $file"
   echo
