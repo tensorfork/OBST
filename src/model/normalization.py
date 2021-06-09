994caf71f9dec49e833404e740ebbf34cde19e4c
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-06-09 16:58:24 +0000

    fix(model): use pow instead of rsqrt

--- src/model/normalization.py
+++ src/model/normalization.py
@@ -8,17 +8,23 @@ from ..utils_mtf import shape_crossection
 
 tf1 = tf.compat.v1
 
+idx = [0]
 
-def norm(args: BlockArgs) -> mtf.Tensor:
+def _norm(args: BlockArgs) -> mtf.Tensor:
     block_input = args.tensor
     feature_shape = shape_crossection(block_input.shape, args.params.feature_dims + get_intermediate(args))
     normalized_shape = block_input.shape - (feature_shape - [args.params.head_dim] * ('group' in args))
 
     block_input -= reduce_mean(block_input, output_shape=normalized_shape)
-    scale = [rsqrt(reduce_mean(mtf.square(block_input), output_shape=normalized_shape) + 1e-6), block_input]
+    scale = [mtf.pow(mtf.reduce_mean(mtf.square(block_input), output_shape=normalized_shape) + 1e-6, -0.5), block_input]
     if 'scale' in args:
         scale.append(normal_var(args.params, feature_shape, mean=1))
     block_input = einsum(scale, output_shape=block_input.shape)
     if 'shift' in args:
         block_input += normal_var(args.params, feature_shape, mean=0)
     return block_input
+
+def norm(args):
+    idx[0] += 1
+    with tf1.variable_scope(str(idx[0])):
+        return _norm(args)
