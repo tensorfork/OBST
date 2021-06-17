54148b4af2c820ef71d6dab2dc88a3e2542339d5
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-06-17 09:51:11 +0200

    style(tests): remove wrapper from base

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -20,13 +20,8 @@ class BaseTest:
                  layout_rules: typing.Union[None, list, str] = None,
                  devices: typing.Union[None, typing.List[str]] = None,
                  **kwargs):
-        with open("tests/test_config.json", 'r') as f:
-            config = f.read()
-        config = jsonpickle.loads(config)
-        params = ModelParameter(config)
-        self.args = BlockArgs(params, None, [''])
-        params.mesh_shape = self.mesh_shape = [] if mesh_shape is None else mesh_shape
-        params.layout = self.layout_rules = [] if layout_rules is None else layout_rules
+        self.mesh_shape = [] if mesh_shape is None else mesh_shape
+        self.layout_rules = [] if layout_rules is None else layout_rules
         self.devices = ["cpu:0"] if devices is None else devices
 
         self.session_config = tf1.ConfigProto()
@@ -37,27 +32,21 @@ class BaseTest:
         if default_session is not None:
             default_session.close()
 
-    def build(self, inp: mtf.Tensor, *args, **kwargs) -> typing.Tuple[typing.List[mtf.Tensor], typing.Any]:
+    def build(self, graph: mtf.Graph, mesh: mtf.Mesh, *args, **kwargs) -> typing.Tuple[
+        typing.List[mtf.Tensor], typing.Any]:
         pass
 
     def run(self, sess: tf1.Session, outputs: typing.List[tf.Tensor], args: typing.Any) -> None:
         pass
 
-    def __call__(self,
-                 dim_size: int,
-                 dim_count: int,
-                 *args,
-                 **kwargs) -> None:
+    def __call__(self, *args, **kwargs) -> None:
         self._close_session()
 
         with tf.Graph().as_default() as tf_graph, tf1.Session(config=self.session_config, graph=tf_graph) as sess:
-            self.args.params.graph = graph = mtf.Graph()
-            self.args.params.mesh = mesh = mtf.Mesh(graph, "MESH")
+            graph = mtf.Graph()
+            mesh = mtf.Mesh(graph, "MESH")
 
-            inp = tf1.random.normal(shape=[dim_size] * dim_count,
-                                    dtype=self.args.params.variable_dtype.activation_dtype)
-            mtf_shape = [mtf.Dimension(str(i), dim_size) for i in range(dim_count)]
-            outputs, args = self.build(mtf.import_tf_tensor(mesh, inp, shape=mtf_shape), *args, **kwargs)
+            outputs, args = self.build(graph, mesh, *args, **kwargs)
 
             mesh_impl = mtf.placement_mesh_impl.PlacementMeshImpl(self.mesh_shape, self.layout_rules, self.devices)
             lowering = mtf.Lowering(graph, {mesh: mesh_impl})
@@ -71,13 +60,30 @@ class BaseTest:
 
 
 class OperationTest(BaseTest):
+    def __init__(self, *args, **kwargs):
+        super(OperationTest, self).__init__(*args, **kwargs)
+        with open("tests/test_config.json", 'r') as f:
+            config = f.read()
+        self.args = BlockArgs(ModelParameter(jsonpickle.loads(config)), None, [''])
+        self.args.params.layout = self.layout_rules
+        self.args.params.mesh_shape = self.mesh_shape
+
     def _build(self, inp: mtf.Tensor, *args, **kwargs) -> mtf.Tensor:
         pass
 
     def _run(self, out: np.array) -> None:
         pass
 
-    def build(self, inp: mtf.Tensor, *args, **kwargs) -> typing.Tuple[typing.List[mtf.Tensor], typing.Any]:
+    def build(self, graph: mtf.Graph, mesh: mtf.Mesh, dim_size: int, dim_count: int,
+              *args, **kwargs) -> typing.Tuple[typing.List[mtf.Tensor], typing.Any]:
+        self.args.params.mesh = mesh
+        self.args.params.graph = graph
+
+        inp = tf1.random.normal(shape=[dim_size] * dim_count,
+                                dtype=self.args.params.variable_dtype.activation_dtype)
+        mtf_shape = [mtf.Dimension(str(i), dim_size) for i in range(dim_count)]
+        inp = mtf.import_tf_tensor(mesh, inp, shape=mtf_shape)
+
         return [self._build(inp, *args, **kwargs)], None
 
     def run(self, sess: tf1.Session, outputs: typing.List[tf.Tensor], args: typing.Any) -> None:
