b3c05ae8a90dd67bd4a84ef362f61e0fc8c5af5d
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-06-19 09:14:00 +0200

    test(basic_test): add initial construct of MonoLinear test

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -5,7 +5,7 @@ import numpy as np
 import pytest
 import tensorflow as tf
 
-from backend import OperationTest, curry_class
+from backend import OperationTest
 from src.model import basic
 
 tf1 = tf.compat.v1
@@ -31,17 +31,39 @@ class Dropout(OperationTest):
         self._is_close(np.sum(out == 0) / out.size, params.input_dropout, 0.2)
 
 
+class MonoLinear(OperationTest):
+    def _build(self, inp: mtf.Tensor) -> mtf.Tensor:
+        a = basic.wrapped_linear(self.args(inp))
+        print(inp.shape, a.shape)
+        return a
+
+    def _run(self, out: np.array) -> None:
+        self._is_close(np.mean(np.std(out, -1)), 1, 0.2)
+
+
+@pytest.mark.skip
 @pytest.mark.parametrize("test",
-                         [ReZero,
-                          curry_class(Dropout, input_dropout=0), curry_class(Dropout, input_dropout=0.1),
-                          curry_class(Dropout, input_dropout=0.25), curry_class(Dropout, input_dropout=0.5),
-                          curry_class(Dropout, input_dropout=0.9)])
+                         [ReZero, Dropout])
 @pytest.mark.parametrize("calculation_dtype", ["bfloat16", "float32"])
 @pytest.mark.parametrize("storage_dtype", ["bfloat16", "float32"])
 @pytest.mark.parametrize("slice_dtype", ["bfloat16", "float32"])
 @pytest.mark.parametrize("embd_per_head", [1, 16, 256])
 @pytest.mark.parametrize("batch_size", [1, 8, 64, 256])
-def op_test(test: typing.Type, calculation_dtype: str, storage_dtype: str, slice_dtype: str, embd_per_head: int,
-            batch_size: int):
+def pointwise_test(test: typing.Type, calculation_dtype: str, storage_dtype: str, slice_dtype: str, embd_per_head: int,
+                   batch_size: int):
     test(calculation_dtype=calculation_dtype, storage_dtype=storage_dtype, slice_dtype=slice_dtype,
          n_embd_per_head=embd_per_head, n_head=1, batch_size=batch_size, n_ctx=1)()
+
+
+@pytest.mark.parametrize("test",
+                         [MonoLinear])
+@pytest.mark.parametrize("calculation_dtype", ["bfloat16", "float32"])
+@pytest.mark.parametrize("storage_dtype", ["bfloat16", "float32"])
+@pytest.mark.parametrize("slice_dtype", ["bfloat16", "float32"])
+@pytest.mark.parametrize("embd_per_head", [1, 16, 256])
+@pytest.mark.parametrize("heads", [1, 2, 8])
+@pytest.mark.parametrize("group_linear_factor", [1, 2, 8])
+def matmul_test(test: typing.Type, calculation_dtype: str, storage_dtype: str, slice_dtype: str, embd_per_head: int,
+                heads: int, group_linear_factor: int):
+    test(calculation_dtype=calculation_dtype, storage_dtype=storage_dtype, slice_dtype=slice_dtype,
+         n_embd_per_head=embd_per_head, n_head=heads, batch_size=1, n_ctx=1, group_linear_factor=group_linear_factor)()
