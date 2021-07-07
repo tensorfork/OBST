7fb53f6b98b0fbd57337d716960c82e5088d46ca
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-07-07 06:07:12 +0200

    fix(optimizer): import float

--- src/optimizer/optimizers.py
+++ src/optimizer/optimizers.py
@@ -58,8 +58,8 @@ def adaptive_gradient_clipping(ctx: OptimizerCtx, gradient_clip: str) -> mtf.Ten
     gradient_clip = float(gradient_clip)
     grd_norm = sqrt_eps(einsum([ctx.grad, ctx.grad], reduced_dims=get_fan_in(ctx.params, ctx.var)))
     wgt_norm = sqrt_eps(einsum([ctx.var.value, ctx.var.value], reduced_dims=get_fan_in(ctx.params, ctx.var)))
-    return weighted_add(einsum([grd_norm, reciprocal(wgt_norm), gradient_clip, ctx.grad], output_shape=ctx.grad.shape),
-                        ctx.grad,
+    return weighted_add(einsum([grd_norm, reciprocal(wgt_norm), optimizer_scalar(ctx.params, gradient_clip), ctx.grad],
+                               output_shape=ctx.grad.shape), ctx.grad,
                         cast(greater(multiply(wgt_norm, reciprocal(grd_norm)), gradient_clip),
                              ctx.params.optimizer_calculation_dtype))
 
