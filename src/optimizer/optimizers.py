e85c3ade4f79dd17d9dbd2b0cd9911dc3b259981
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-07-07 19:59:23 +0200

    fix(optimizer): add bfp16 support to agc

--- src/optimizer/optimizers.py
+++ src/optimizer/optimizers.py
@@ -4,7 +4,7 @@ from .backend import variable
 from .context import OptimizerCtx
 from ..mtf_wrapper import (cast, optimizer_scalar, einsum, greater, minimum,
                            reduce_mean, reduce_sum, assign, add, multiply, maximum, sqrt_eps, reciprocal, square,
-                           reduce_max, rsqrt, sqrt, pow, negative)
+                           reduce_max, rsqrt, sqrt)
 from ..utils_mtf import weighted_add, get_fan_in
 
 
@@ -57,7 +57,8 @@ def return_grad(ctx: OptimizerCtx) -> mtf.Tensor:
 def adaptive_gradient_clipping(ctx: OptimizerCtx, gradient_clip: str) -> mtf.Tensor:
     gradient_clip = float(gradient_clip)
     grd_norm = sqrt_eps(einsum([ctx.grad, ctx.grad], reduced_dims=get_fan_in(ctx.params, ctx.var)))
-    wgt_norm = sqrt_eps(einsum([ctx.var.value, ctx.var.value], reduced_dims=get_fan_in(ctx.params, ctx.var)))
+    wgt_norm = sqrt_eps(einsum([cast(ctx.var.value, ctx.params.optimizer_calculation_dtype)] * 2,
+                               reduced_dims=get_fan_in(ctx.params, ctx.var)))
     return weighted_add(einsum([grd_norm, reciprocal(wgt_norm), optimizer_scalar(ctx.params, gradient_clip), ctx.grad],
                                output_shape=ctx.grad.shape), ctx.grad,
                         cast(greater(multiply(wgt_norm, reciprocal(grd_norm)), gradient_clip),
