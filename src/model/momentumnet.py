fix(gradients): add gradient_iterator to split gather/revnet

src/model/momentumnet.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 from .frontend import block_part_fn
 from ..dataclass import ModelParameter
 from ..utils_core import random_name
+from ..utils_mtf import gradient_iterator
 
 tf1 = tf.compat.v1
 
@@ -74,8 +75,8 @@ class MomentumOperation(mtf.Operation):
                     grad_outputs = [tensor_to_gradient.get(out) for out in op.outputs]
                     if not op.has_gradient or not any(grad_outputs) or not set(op.inputs) & downstream:
                         continue
-                    with tf1.variable_scope(op.name + "/revnet/gradients"):
-                        for inp, grad in zip(op.inputs, op.gradient(grad_outputs)):
+                    with tf1.variable_scope(op.name + "/momentumnet/gradients"):
+                        for inp, grad in gradient_iterator(op, grad_outputs):
                             if inp not in downstream or grad is None:
                                 continue
                             if inp in tensor_to_gradient:
@@ -106,13 +107,13 @@ class MomentumOperation(mtf.Operation):
                         del tensor_to_gradient[out]
                 if not op.has_gradient or not any(grad_outputs) or not set(op.inputs) & downstream:
                     continue
-                for inp, grad in zip(op.inputs, op.gradient(grad_outputs)):
+                for inp, grad in gradient_iterator(op, grad_outputs):
                     if inp not in downstream or grad is None:
                         continue
                     if inp in tensor_to_gradient:
                         grad_list = tensor_to_gradient[inp]
                         grad_list[1] += 1
-                        with tf1.variable_scope(op.name + "/revnet/gradients"):
+                        with tf1.variable_scope(op.name + "/momentumnet/gradients"):
                             grad_list[2] += grad
                     else:
                         tensor_to_gradient[inp] = grad_list = [0, 1, grad]

src/model/revnet.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 from .frontend import block_part_fn
 from ..utils_core import random_name
 from ..mtf_wrapper import add
-
+from ..utils_mtf import gradient_iterator
 tf1 = tf.compat.v1
 
 
@@ -70,7 +70,7 @@ class RevGradOp(mtf.Operation):
                     if not op.has_gradient or not any(grad_outputs) or not set(op.inputs) & downstream:
                         continue
                     with tf1.variable_scope(op.name + "/revnet/gradients"):
-                        for inp, grad in zip(op.inputs, op.gradient(grad_outputs)):
+                        for inp, grad in gradient_iterator(op, grad_outputs):
                             if inp not in downstream or grad is None:
                                 continue
                             if inp in tensor_to_gradient:
@@ -99,7 +99,7 @@ class RevGradOp(mtf.Operation):
                         del tensor_to_gradient[out]
                 if not op.has_gradient or not any(grad_outputs) or not set(op.inputs) & downstream:
                     continue
-                for inp, grad in zip(op.inputs, op.gradient(grad_outputs)):
+                for inp, grad in gradient_iterator(op, grad_outputs):
                     if inp not in downstream or grad is None:
                         continue
                     if inp in tensor_to_gradient:

src/optimizer/__init__.py
@@ -16,12 +16,10 @@ from .learning_rate import get_learning_rate
 from .optimizers import OPTIMIZERS
 from ..dataclass import ModelParameter
 from ..model.embedding import Gather
-from ..model.revnet import RevGradOp
-from ..mtf_wrapper import (cast, constant_float, constant_scalar, einsum, equal, greater_equal, mod,
-                           reduce_sum, assign, assign_sub,
-                           add, multiply, scoped, assign_add, identity, zeros_like, negative, optimizer_scalar,
-                           reciprocal, reduce_mean, broadcast, reshape)
-from ..utils_mtf import feature_dims_used, to_fp32, random_name
+from ..mtf_wrapper import (cast, constant_float, constant_scalar, einsum, equal, greater_equal, mod, reduce_sum, assign,
+                           assign_sub, add, multiply, scoped, assign_add, identity, zeros_like, negative,
+                           optimizer_scalar, reciprocal, reduce_mean, broadcast, reshape)
+from ..utils_mtf import feature_dims_used, to_fp32, random_name, gradient_iterator
 
 tf = tf2.compat.v1
 zeros = tf.zeros_initializer()
@@ -191,13 +189,7 @@ def get_optimizer(loss_list: typing.List[mtf.Tensor], params: ModelParameter, ma
 
                 if not op.has_gradient or not any(grad_outputs) or not (set(op.inputs) & downstream):
                     continue
-                if isinstance(op, RevGradOp):
-                    itr = op.gradient(grad_outputs, params=op.inputs)
-                elif isinstance(op, Gather):
-                    itr = ((op.inputs[1], grad_outputs[0]),)
-                else:
-                    itr = zip(op.inputs, op.gradient(grad_outputs))
-                for inp, grad in itr:
+                for inp, grad in gradient_iterator(op, grad_outputs):
                     if inp not in downstream or grad is None:
                         continue
 

src/utils_mtf.py
@@ -5,8 +5,12 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.python.ops.init_ops import Initializer
 
+from model.embedding import Gather
+from model.momentumnet import MomentumOperation
+from model.revnet import RevGradOp
 from .dataclass import BlockArgs, ModelParameter
-from .mtf_wrapper import cast, mtf_range, reshape, concat as mtf_concat, pad as mtf_pad, mtf_slice, add, multiply, negative
+from .mtf_wrapper import cast, mtf_range, reshape, concat as mtf_concat, pad as mtf_pad, mtf_slice, add, multiply, \
+    negative
 from .utils_core import default, random_name
 
 tf1 = tf.compat.v1
@@ -176,6 +180,15 @@ def deduplicate(inp: SHAPE) -> SHAPE:
     :return: new, unique iterable of same type as input
     """
     return type(inp)(dict.fromkeys(list(inp)))
+
+
+def gradient_iterator(op: mtf.Operation, grad_outputs: typing.List[mtf.Tensor]
+                      ) -> typing.Iterable[typing.Tuple[mtf.Tensor, mtf.Tensor]]:
+    if isinstance(op, Gather):
+        return (op.inputs[1], grad_outputs[0]),
+    if isinstance(op, (RevGradOp, MomentumOperation)):
+        return op.gradient(grad_outputs, params=op.inputs)
+    return zip(op.inputs, op.gradient(grad_outputs))
 
 
 def anonymize(inp: mtf.Tensor,
@@ -403,6 +416,7 @@ def get_fan_in(params: ModelParameter, shape: ALL_SHAPES) -> DIM_LIST:
     if features_used:
         return shape[:2]
     return shape[:1]
+
 
 # The majority of this Function was copied from:
 # 'https://github.com/tensorflow/mesh/blob/8931eb9025f833b09d8425404ebd5801acbb0cac/mesh_tensorflow/ops.py#L5956-L6104'