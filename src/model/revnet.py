fix(embedding): more lr multiplier to gradient stage

src/model/revnet.py
@@ -4,9 +4,10 @@ import mesh_tensorflow as mtf
 import tensorflow as tf
 
 from .frontend import block_part_fn
-from ..utils_core import random_name
 from ..mtf_wrapper import add
+from ..utils_core import random_name
 from ..utils_mtf import gradient_iterator
+
 tf1 = tf.compat.v1
 
 
@@ -70,7 +71,7 @@ class RevGradOp(mtf.Operation):
                     if not op.has_gradient or not any(grad_outputs) or not set(op.inputs) & downstream:
                         continue
                     with tf1.variable_scope(op.name + "/revnet/gradients"):
-                        for inp, grad in gradient_iterator(op, grad_outputs):
+                        for inp, grad in gradient_iterator(self.params, op, grad_outputs):
                             if inp not in downstream or grad is None:
                                 continue
                             if inp in tensor_to_gradient:
@@ -99,7 +100,7 @@ class RevGradOp(mtf.Operation):
                         del tensor_to_gradient[out]
                 if not op.has_gradient or not any(grad_outputs) or not set(op.inputs) & downstream:
                     continue
-                for inp, grad in gradient_iterator(op, grad_outputs):
+                for inp, grad in gradient_iterator(self.params, op, grad_outputs):
                     if inp not in downstream or grad is None:
                         continue
                     if inp in tensor_to_gradient: