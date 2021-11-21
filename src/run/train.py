39d0bb3bab4477c7de97ec4385c20279e926a4bc
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-11-21 13:23:46 +0100

    fix(run/train): cast loss to fp64

--- src/run/train.py
+++ src/run/train.py
@@ -1,5 +1,3 @@
-import typing
-
 import mesh_tensorflow as mtf
 import tensorflow as tf
 
@@ -7,7 +5,7 @@ from ..dataclass import ModelParameter
 from ..model import build
 from ..mtf_wrapper import constant_scalar
 from ..optimizer import get_optimizer
-from ..utils_mtf import WhileLoopWithControlDependencies, utils_slice
+from ..utils_mtf import WhileLoopWithControlDependencies
 
 
 def get_train_model(params: ModelParameter):
@@ -35,7 +33,7 @@ def get_train_model(params: ModelParameter):
                token_loss, accuracy, update_ops, debug_gradients_dict
 
     def train_in_loop(frame_input, cat_mask_src, cat_mask_tag, token_x_input, token_y_input,
-                    frame_mask_src, frame_mask_tag, token_mask, manual_global_step):
+                      frame_mask_src, frame_mask_tag, token_mask, manual_global_step):
 
         loop_input = [constant_scalar(params, 0, dtype=tf.int64),
                       constant_scalar(params, 0, dtype=tf.float32),
@@ -44,16 +42,15 @@ def get_train_model(params: ModelParameter):
                       constant_scalar(params, 0, dtype=params.calculation_dtype),
                       constant_scalar(params, 0, dtype=params.calculation_dtype)]
 
+        def inp_slice_fn(x: mtf.Tensor):
+            if x is not None:
+                slice_dim = mtf.Dimension("batch_slice", params.macro_batching)
+                return mtf.gather(mtf.replace_dimensions(x, params.macro_batch_dim, [params.batch_dim, slice_dim]),
+                                  params.macro_batching, slice_dim)
+
+            return x
+
         def body_fn(idx, loss, learning_rate, video_loss, token_loss, accuracy):
-
-            def inp_slice_fn(x: mtf.Tensor):
-                if x is not None:
-                    slice_dim = mtf.Dimension("batch_slice", params.macro_batching)
-                    return mtf.gather(mtf.replace_dimensions(x, params.macro_batch_dim, [params.batch_dim, slice_dim]),
-                        params.macro_batching, slice_dim)
-
-                return x
-
             frame_out, token_out, _learning_rate, _loss, _video_loss, \
             _token_loss, _accuracy, update_ops, debug_gradients_dict = train_model(inp_slice_fn(frame_input),
                                                                                    inp_slice_fn(cat_mask_src),
@@ -66,31 +63,28 @@ def get_train_model(params: ModelParameter):
                                                                                    idx)
 
             if _loss is not None:
-                loss = _loss
+                loss = mtf.cast(_loss, tf.float64)
             if _learning_rate is not None:
                 learning_rate = _learning_rate
             if _video_loss is not None:
-                video_loss = _video_loss
+                video_loss = mtf.cast(_video_loss, tf.float64)
             if _token_loss is not None:
-                token_loss = _token_loss
+                token_loss = mtf.cast(_token_loss, tf.float64)
             if _accuracy is not None:
-                accuracy = _accuracy
+                accuracy = mtf.cast(_accuracy, tf.float64)
 
             return {"outputs": [mtf.add(idx, constant_scalar(params, 1, dtype=tf.int64), name="idx_add_one"), loss,
-                    learning_rate, video_loss, token_loss, accuracy],
+                                learning_rate, video_loss, token_loss, accuracy],
                     "control_dependencies": update_ops}
 
         def count_fn(idx, *args):
             return mtf.less(idx, constant_scalar(params, params.macro_batching, dtype=tf.int64))
 
-        _, loss,\
-        learning_rate,\
-        video_loss,\
-        token_loss,\
+        _, loss, \
+        learning_rate, \
+        video_loss, \
+        token_loss, \
         accuracy = WhileLoopWithControlDependencies(cond_fn=count_fn, body_fn=body_fn, inputs=loop_input).outputs
-
-
-
 
         return None, None, learning_rate, loss, video_loss, token_loss, accuracy, [], None
 
