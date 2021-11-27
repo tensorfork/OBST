fix(model): accumulate unsparsified gradient

src/optimizer/__init__.py
@@ -15,11 +15,10 @@ from .gradients import MULTI_LOSS_GRADIENTS
 from .learning_rate import get_learning_rate
 from .optimizers import OPTIMIZERS
 from ..dataclass import ModelParameter
-from ..model.embedding import Gather
 from ..mtf_wrapper import (cast, constant_float, constant_scalar, einsum, equal, greater_equal, mod, reduce_sum, assign,
                            add, multiply, scoped, identity, zeros_like, negative, optimizer_scalar, reciprocal,
                            reduce_mean, broadcast, assign_sub, assign_add)
-from ..utils_mtf import feature_dims_used, to_fp32, gradient_iterator, scatter_add
+from ..utils_mtf import feature_dims_used, to_fp32, gradient_iterator
 
 tf = tf2.compat.v1
 zeros = tf.zeros_initializer()
@@ -160,14 +159,9 @@ def get_optimizer(loss_list: typing.List[mtf.Tensor], params: ModelParameter, ma
                     if inp in tensor_to_gradient:
                         grad_list = tensor_to_gradient[inp]
                         grad_list[1] += 1
+                        grad_list[2] += grad
                         grad_list[3] = inner_op
-                        if isinstance(inner_op, Gather):
-                            grad_list[2] = scatter_add(grad_list[2], inner_op.inputs[0], grad)
-                        else:
-                            grad_list[2] += grad
                     else:
-                        if isinstance(inner_op, Gather):
-                            grad = scatter_add(mtf.zeros(params.mesh, inp.shape, inp.dtype), inner_op.inputs[0], grad)
                         tensor_to_gradient[inp] = [0, 1, grad, inner_op]
 
     ctx = OptimizerCtx(op, grad_outputs, downstream, tensor_to_gradient, tensor_to_var, params,

src/run/utils_run.py
@@ -6,10 +6,10 @@ import tensorflow as tf
 from tensorflow.python.ops import summary_ops_v2 as summary
 from tensorflow.python.tpu import tpu
 
+from .. import tf_wrapper as tfw
 from ..dataclass import ModelParameter
 from ..mtf_wrapper import import_laid_out_tensor
 from ..utils_core import color_print
-from .. import tf_wrapper as tfw
 
 tf1 = tf.compat.v1
 Dataset = tf1.data.Dataset
@@ -68,15 +68,18 @@ def analyze_model(params: ModelParameter, time_to_build: float, graph: mtf.Graph
     var_count = int(sum([variable.size for variable in graph.all_variables]))
     embed_param_count = int(sum([variable.size for variable in
                                  graph.trainable_variables if 'embed' in variable.name]))
+    gather_param_count = int(sum([variable.size for variable in
+                                  graph.trainable_variables if 'gather' in variable.name]))
     body_param_count = int(sum([variable.size for variable in
                                 graph.trainable_variables if 'body' in variable.name]))
 
     print('')
 
     constant = '  variables: '
-    variable_mapping = [('Model', param_count - embed_param_count),
-                        ('Gather', embed_param_count),
-                        ('Body with Embed', body_param_count),
+    variable_mapping = [('Core', param_count - embed_param_count),
+                        ('Embedding', embed_param_count - gather_param_count),
+                        ('Sparse', gather_param_count),
+                        ('Full Model', body_param_count),
                         ('Untrainable', var_count - param_count),
                         ('', 0),
                         ('Total trainable', param_count),
@@ -111,7 +114,6 @@ def analyze_model(params: ModelParameter, time_to_build: float, graph: mtf.Graph
 
 
 def rep_batch(params: ModelParameter, shape: [mtf.Shape, typing.List[mtf.Dimension]]):
-
     if params.macro_batching > 1 and params.train:
         return mtf.replace_dimensions(shape, params.batch_dim, params.macro_batch_dim)
     return shape