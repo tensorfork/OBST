c9df16594f827e032d7f2d656734d87fa2d5bc3e
ClashLuke <39779310+ClashLuke@users.noreply.github.com>
2021-11-28 14:00:36 +0100

    fix(model/norm): actually do proxynorm

--- src/model/normalization.py
+++ src/model/normalization.py
@@ -1,7 +1,9 @@
 import typing
 
 import mesh_tensorflow as mtf
+import numpy as np
 import tensorflow as tf
+from scipy.special import erfinv
 
 from .backend import normal_var, SHAPE
 from ..dataclass import BlockArgs
@@ -11,23 +13,33 @@ from ..utils_mtf import linear_shapes
 tf1 = tf.compat.v1
 
 
+def uniformly_sampled_gaussian(num_rand):
+    rand = 2 * (np.arange(num_rand) + 0.5) / float(num_rand) - 1
+    return np.sqrt(2) * erfinv(rand)
+
+
 def norm(args: BlockArgs, feature_shape: typing.Optional[SHAPE] = None) -> mtf.Tensor:
     block_input = args.tensor
     feature_shape = mtf.Shape(linear_shapes(args).old if feature_shape is None else feature_shape)
     normalized_shape = block_input.shape - (feature_shape - [args.params.head_dim] * ('group' in args))
+
+    scale = normal_var(args, feature_shape, mean=1) if 'scale' in args else None
+    shift = normal_var(args, feature_shape, mean=0) if 'shift' in args else None
+
     if 'proxy' in args:
-        base = normal_var(args, feature_shape, stddev=1, mean=0)
-        sub = reduce_mean(base, output_shape=[])
-        base -= sub
-        block_input -= sub
-        div = rsqrt_eps(reduce_mean(square(base), output_shape=[]), 1e-5)
+        proxy_z = mtf.constant(block_input.mesh, uniformly_sampled_gaussian(args.params.train_batch_size),
+                               [args.params.batch_dim], dtype=block_input.dtype)
+        proxy_z *= scale
+        proxy_z += shift
+        sub = reduce_mean(proxy_z, output_shape=[])
+        proxy_z -= sub
+        block_input -= shift
+        block_input /= scale
+        block_input -= proxy_z
+        block_input /= rsqrt_eps(reduce_mean(square(proxy_z), output_shape=[]), 1e-5)
     else:
         block_input -= reduce_mean(block_input, output_shape=normalized_shape)
         div = rsqrt_eps(reduce_mean(square(block_input), output_shape=normalized_shape), 1e-5)
-    scale = [div, block_input]
-    if 'scale' in args:
-        scale.append(normal_var(args, feature_shape, mean=1))
-    block_input = einsum(scale, output_shape=block_input.shape)
-    if 'shift' in args:
-        block_input += normal_var(args, feature_shape, mean=0)
+        scale = [div, block_input]
+        block_input = einsum(scale, output_shape=block_input.shape)
     return block_input
